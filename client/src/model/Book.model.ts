0f12701cf0bce80cbf3c5187f0548d297e75790a
miljanv99 <miljanvukovic85@gmail.com>
2024-10-26 18:49:00 +0200

    change book model

--- client/src/model/Book.model.ts
+++ client/src/model/Book.model.ts
@@ -1,19 +1,19 @@
 export type Book = {
-  _id?: string;
-  title?: string;
-  author?: string;
-  cover?: string;
-  description?: string;
-  currentRating?: number;
+  _id: string;
+  title: string;
+  author: string;
+  cover: string;
+  description: string;
+  currentRating: number;
   ratingPoints?: number;
-  retedCount?: number;
-  //ratedBy?:
+  ratedCount?: number;
+  //ratedBy:
   purchasesCount?: number;
-  //comments?:
-  genre?: string;
-  year?: number;
+  //comments:
+  genre: string;
+  year: number;
   isbn?: string;
-  pagesCount?: number;
-  price?: number;
+  pagesCount: number;
+  price: number;
   creationDate?: Date;
 };
